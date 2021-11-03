e34682619fc4db8b47160007058545b409003de9
Adam <adammoore118@outlook.com>
2021-11-03 17:11:57 -0400

    commit tree 37182aae307ac6e01c0351fe9ee593a8428b3475

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     if array.length == 1
+#         array.join(" ")
+#     elsif  array.length == 2
+#         array.join(" and ")
+#     else
+#         array[-1] = "and #{array[-1]}"
+#         array.join(", ")
+#     end
+# end
 
-end
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+  
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ")  
+  end
